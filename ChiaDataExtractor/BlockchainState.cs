6ee539dfcfede11b56d3017cd4a2f30447c6d2f6
Eddy Boughioul <eddyboughioul@hotmail.fr>
2021-06-03 00:24:40 +0200

    Ajoutez des fichiers projet.

---
+++ ChiaDataExtractor/BlockchainState.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChiaDataExtractor
+{
+    public class ChallengeVdfOutput
+    {
+        public string data { get; set; }
+    }
+
+    public class InfusedChallengeVdfOutput
+    {
+        public string data { get; set; }
+    }
+
+    public class Peak
+    {
+        public string challenge_block_info_hash { get; set; }
+        public ChallengeVdfOutput challenge_vdf_output { get; set; }
+        public int deficit { get; set; }
+        public string farmer_puzzle_hash { get; set; }
+        public object fees { get; set; }
+        public object finished_challenge_slot_hashes { get; set; }
+        public object finished_infused_challenge_slot_hashes { get; set; }
+        public object finished_reward_slot_hashes { get; set; }
+        public string header_hash { get; set; }
+        public int height { get; set; }
+        public InfusedChallengeVdfOutput infused_challenge_vdf_output { get; set; }
+        public bool overflow { get; set; }
+        public string pool_puzzle_hash { get; set; }
+        public string prev_hash { get; set; }
+        public object prev_transaction_block_hash { get; set; }
+        public int prev_transaction_block_height { get; set; }
+        public int required_iters { get; set; }
+        public object reward_claims_incorporated { get; set; }
+        public string reward_infusion_new_challenge { get; set; }
+        public int signage_point_index { get; set; }
+        public object sub_epoch_summary_included { get; set; }
+        public int sub_slot_iters { get; set; }
+        public object timestamp { get; set; }
+        public long total_iters { get; set; }
+        public int weight { get; set; }
+    }
+
+    public class Sync
+    {
+        public bool sync_mode { get; set; }
+        public int sync_progress_height { get; set; }
+        public int sync_tip_height { get; set; }
+        public bool synced { get; set; }
+    }
+
+    public class BlockchainState
+    {
+        public int difficulty { get; set; }
+        public bool genesis_challenge_initialized { get; set; }
+        public int mempool_size { get; set; }
+        public Peak peak { get; set; }
+        public string space { get; set; }
+        public int sub_slot_iters { get; set; }
+        public Sync sync { get; set; }
+    }
+
+    public class RootBlockchainState
+    {
+        public BlockchainState blockchain_state { get; set; }
+        public bool success { get; set; }
+    }
+}
---
+++ ChiaDataExtractor/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Net.WebSockets;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Net.WebSockets;
+using System.Security.Cryptography.X509Certificates;
+using Websocket.Client;
+using System.Net;
+using System.Net.Http;
+using System.Security.Authentication;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using OpenSSL.X509Certificate2Provider;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ChiaDataExtractor
+{
+    class Program
+    {
+        //  static HttpClient client = new HttpClient();
+        //  static HttpClient client = new HttpClient();
+
+        static void Main(string[] args)
+        {
+
+            System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+            Console.OutputEncoding = Encoding.GetEncoding(1252);
+            var folder = ConfigurationManager.AppSettings["folder"];
+
+            ChiaService service = new ChiaService();
+
+            rpcCall();
+
+            //    GetChiaInfos(folder, "show -s");
+            // GetChiaInfos(folder, "farm summary");
+            // GetChiaInfos(folder, "wallet show");
+            Console.ReadLine();
+        }
+
+
+        async static void rpcCall()
+        {
+            try
+            {
+
+                var url = new Uri("wss://localhost:55400");
+                var exitEvent = new ManualResetEvent(false);
+
+
+
+                string certificateFile = @"C:\Users\Eddy\.chia\mainnet\config\ssl\full_node\private_full_node.crt";
+                string keyFile = @"C:\Users\Eddy\.chia\mainnet\config\ssl\full_node\private_full_node.key";
+
+                string cert = File.ReadAllText(certificateFile);
+                string key = File.ReadAllText(keyFile);
+
+                ICertificateProvider provider = new CertificateFromFileProvider(cert, key);
+
+                var publicCertificate = X509Certificate2.CreateFromPemFile(certificateFile, keyFile);
+
+                using (var tmpCert = new X509Certificate2(publicCertificate.Export(X509ContentType.Pfx)))
+                {
+                    var col = new X509Certificate2Collection();
+                    col.Add(tmpCert);
+                    var factory = new Func<ClientWebSocket>(() => new ClientWebSocket
+                    {
+                        Options =
+                    {
+                        KeepAliveInterval = TimeSpan.FromSeconds(5),
+                        ClientCertificates = col,
+                        RemoteCertificateValidationCallback= (requestMessage, certificate, chain, policyErrors) => true
+                    }
+                    });
+                    using (var client = new WebsocketClient(url, factory))
+                    {
+                        client.ReconnectTimeout = TimeSpan.FromSeconds(30);
+                        client.ReconnectionHappened.Subscribe(info =>
+                            Console.WriteLine($"Reconnection happened, type: {info.Type}"));
+
+                        client.MessageReceived.Subscribe(msg => Console.WriteLine($"Message received: {msg}"));
+                        await client.Start();
+                        CommandChia cmd = new CommandChia() { ack = false, command = "get_status", service= "chia", data = new Data() { value = "pong" }, request_id = "123456", destination = "wallet", origin = "ui" };
+                        string jsonString = JsonSerializer.Serialize(cmd);
+                        await Task.Run(() => client.Send(jsonString));
+
+                        exitEvent.WaitOne();
+                    }
+
+
+
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error" + ex.StackTrace);
+            }
+        }
+
+        static byte[] ReadFully(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+
+        private static void GetChiaInfos(string folder, string args)
+        {
+            try
+            {
+                var process = new Process
+                {
+                    StartInfo = new ProcessStartInfo
+                    {
+                        FileName = Path.Combine(folder, "chia.exe"),
+                        Arguments = args,
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        StandardOutputEncoding = Encoding.GetEncoding(1252),
+                        RedirectStandardError = true,
+                        StandardErrorEncoding = Encoding.GetEncoding(1252),
+                        CreateNoWindow = true
+                    }
+                };
+
+                process.Start();
+
+                while (!process.StandardOutput.EndOfStream)
+                {
+                    var line = process.StandardOutput.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+
+
+                while (!process.StandardError.EndOfStream)
+                {
+                    var line = process.StandardError.ReadToEnd();
+                    Console.WriteLine(line);
+                }
+
+                //byte[] result = ReadFully(process.StandardOutput.BaseStream);
+
+                //foreach (System.Text.EncodingInfo encodingInfo in System.Text.Encoding.GetEncodings())
+                //{
+                //    System.Text.Encoding encoding = encodingInfo.GetEncoding();
+                //    string decodedBytes = encoding.GetString(result);
+                //    System.Console.Out.WriteLine("Encoding: {0}, Decoded Bytes: {1}", encoding.EncodingName, decodedBytes);
+                //}
+
+
+
+                process.WaitForExit();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+        }
+    }
+
+    public class CommandChia
+    {
+        public string command { get; set; }
+        public bool ack { get; set; }
+        public Data data { get; set; }
+        public string request_id { get; set; }
+        public string destination { get; set; }
+        public string origin { get; set; }
+        public string service { get; set; }
+    }
+
+    public class Data
+    {
+        public string value { get; set; }
+    }
+
+
+}
